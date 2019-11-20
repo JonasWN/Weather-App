6195154267bebd443f860a41db096592f429554c
Your Name <you@example.com>
2019-11-20 23:06:16 +0100

    First version layout done

--- Weather-App/dist/assets/js/current-weather.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-fetch("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={68f82a388c4c0de995710dfc8a8ca667}").then(function (answer) {
-  return answer.json();
-}).then(function (response) {
-  console.log(response);
-});
